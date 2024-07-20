a4ca78b44ce424532bb8e1a8ce017843a896cf29
Barnaby Taylor <github.ablaze823@passinbox.com>
2024-07-20 23:06:17 +0100

    Add link to event list to navbar

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.event_list, name='event_list'),
-    path('event/<int:event_id>/', views.event_detail, name='event_detail'),
+    path('event_detail/', views.event_detail, name='event_detail'),
 ]
